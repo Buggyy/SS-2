d07a58fafa40a62f5fadfcc079d646ffb0b78d0e
Buggyy <stefan.lobato.yildiz@hva.nl>
2015-11-25 12:14:23 +0100

    initial commit

---
+++ src/ss/pkg2/SS2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ss.pkg2;
+
+/**
+ *
+ * @author Ömer Zülaloğlu [IS204] 500712124
+ * @author Stefan Lobato [IS204] 500707274
+ */
+public class SS2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
